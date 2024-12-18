9d139b5ab4a5a871e507f7487c466eba29baf01d
arun420786 <karansahani243@gmail.com>
2024-12-18 11:36:33 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log("welcome to spotify");
+let songs = [
+    {songname : "salam-e-ishq", filepath : "song/Tell me truth", coverpath : "covers/cover img.jpeg"}
+]
+let audioElement = new Audio('tell me truth');
+// audioElement.play();
+
+
+
+
+
